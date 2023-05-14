7ed1be09d5043931d8e999a41a870f1dc09b27ec
Aaron Johnson <awesomeaaron2000@gmail.com>
2023-05-14 13:43:29 -0700

    flask structure and layout

--- TOTD.py
+++ TOTD.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 
 @app.route('/')
 def home():
-    return 'Hello, world!'
+    return render_template('index.html')
