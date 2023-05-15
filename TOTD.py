e36976bf2de25e75247549e6e0b1dac35f6efc91
Aaron Johnson <awesomeaaron2000@gmail.com>
2023-05-15 15:11:30 -0700

    Added proper documentation

--- TOTD.py
+++ TOTD.py
@@ -1,3 +1,32 @@
+""" Introduction:
+Course: CST 205 Section 1 - Multimedia Design & Programming
+
+Title: TOTD (TRENDS OF THE DAY)
+
+Abstract: This is a flask app to show daily trends of the top 
+platforms on the internet to enhance marketing.
+
+Authors: Aaron Johnson, Eunice Sosa, Sebastian Ramos, Mikaela Lagumbay
+
+Date: May 18, 2023
+
+Work by Aaron Johnson:
+1. Researched and found Trend info for several platforms
+2. Created the flask app
+3. Created the basic structure of the index.html file
+4. Used some boostrap to make the website look better
+
+Work by Eunice Sosa:
+1. Researched and found Trend info for several platforms
+
+Work by Sebastian Ramos:
+1. Researched and found Trend info for several platforms
+
+Work by Mikaela Lagumbay:
+1. Researched and found Trend info for several platforms
+
+"""
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
