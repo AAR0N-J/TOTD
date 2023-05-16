0df7934ae6738f4aa318171a6d271682973ac6cc
Aaron Johnson <awesomeaaron2000@gmail.com>
2023-05-16 14:30:04 -0700

    Spotify trends done

--- TOTD.py
+++ TOTD.py
@@ -40,12 +40,32 @@ bootstrap = Bootstrap5(app)
 
 # API Section
 
+# SPOTIFY
+ssl._create_default_https_context = ssl._create_unverified_context
+spotify_daily_site = 'https://kworb.net/spotify/country/us_daily.html'
+spotify_daily_soup = BeautifulSoup(urlopen(spotify_daily_site).read(), 'html.parser')
+spotify_top = spotify_daily_soup.find('td',{'class':'text mp'}).get_text().split('-')
+spotify_artist_site = 'https://kworb.net/spotify/artists.html'
+spotify_top_soup = BeautifulSoup(urlopen(spotify_artist_site).read(), 'html.parser')
+top_artist = spotify_top_soup.find_all('td')[0].get_text()
+spotify_song_site = 'https://kworb.net/spotify/toplists.html'
+spotify_song_soup = BeautifulSoup(urlopen(spotify_song_site).read(), 'html.parser')
+top_song = spotify_song_soup.find_all('td')[1].get_text().split('-')
+spotify_info = {
+    'song' : spotify_top[1],
+    'artist' : spotify_top[0],
+    'top_artist' : top_artist,
+    'top_song_title' : top_song[1],
+    'top_song_artist' : top_song[0]
+}
+# END SPOTIFY
+
 #END API Section
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', spotify=spotify_top[0])
 
-@app.route('/googleTrends')
-def googleTrends():
-    return render_template('googleTrends.html')
+@app.route('/spotifyTrends')
+def spotifyTrends():
+    return render_template('spotifyTrends.html', spotify=spotify_info)
