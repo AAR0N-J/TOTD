8c2ff82b7e64b9c8d4ad5a45ee67b74130127512
Sebastian Ramos <seramos@csumb.edu>
2023-05-16 21:02:28 -0700

    youtube trending donegit add .

--- TOTD.py
+++ TOTD.py
@@ -5,7 +5,6 @@ Title: TOTD (TRENDS OF THE DAY)
 
 Abstract: This is a flask app to show daily trends of the top 
 platforms on the internet to enhance marketing.
-
 Authors: Aaron Johnson, Eunice Sosa, Sebastian Ramos, Mikaela Lagumbay
 
 Date: May 18, 2023
@@ -59,13 +58,46 @@ spotify_info = {
     'top_song_artist' : top_song[0]
 }
 # END SPOTIFY
+# YOUTUBE
+api_key = 'AIzaSyDXZ2OVo42hco-qB5fsjfLg_V9bJLyqX70'
 
+def get_trending_video(api_key):
+    url = 'https://www.googleapis.com/youtube/v3/videos'
+    params = {
+        'part': 'snippet',
+        'chart': 'mostPopular',
+        'regionCode': 'US',
+        'maxResults': 1,
+        'key': api_key
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if 'items' in data and len(data['items']) > 0:
+        video = data['items'][0]
+        video_info = {
+            'title': video['snippet']['title'],
+            'video_id': video['id'],
+            'thumbnail': video['snippet']['thumbnails']['default']['url']
+        }
+        return video_info
+
+    return None
+
+youtube_trending = get_trending_video(api_key)
+
+# END YOUTUBE
 #END API Section
 
 @app.route('/')
 def home():
-    return render_template('index.html', spotify=spotify_top[0])
+    return render_template('index.html', spotify=spotify_top[0],video_title=youtube_trending['title'])
 
 @app.route('/spotifyTrends')
 def spotifyTrends():
     return render_template('spotifyTrends.html', spotify=spotify_info)
+
+@app.route('/youtube_trends')
+def youtube_trends():
+    return render_template('youtubeTrends.html', youtube=youtube_trending)
